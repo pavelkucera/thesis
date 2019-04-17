2c2cd8a5f3d1ffbb8c9483255dd07af62381056c
Pavel Kučera <pave.kucera@gmail.com>
2019-04-17 15:02:36 +0200

    Replace it...property by prop

--- test/TypesSpec.hs
+++ test/TypesSpec.hs
@@ -3,6 +3,7 @@ module TypesSpec (spec) where
 import Types
 import Data.Either (isRight)
 import Test.Hspec
+import Test.Hspec.QuickCheck
 import Test.QuickCheck
 
 spec :: Spec
@@ -11,12 +12,12 @@ spec = describe "types" $ do
       prop "returns an Epsilon when the input is positive" $
         \(Positive e) -> isRight (epsilon e)
 
-      it "returns an error when the input is not positive" $ property $
+      prop "returns an error when the input is not positive" $
         \(NonNegative e) -> (epsilon (-1 * e)) `shouldBe` Left (NonPositiveEpsilon (-1 * e))
 
   describe "delta" $ do
       prop "returns a Delta when the input is not negative" $
         \(NonNegative d) -> isRight (delta d)
 
-      it "returns an error when the input is negative" $ property $
+      prop "returns an error when the input is negative" $
         \(Positive d) -> (delta (-1 * d)) `shouldBe` Left (NegativeDelta (-1 * d))
