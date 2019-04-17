812d07548d6fd5800f946dfc3b0e63e05d1227bb
Pavel Kučera <pave.kucera@gmail.com>
2019-04-17 15:01:13 +0200

    Remove duplicate describe

--- test/TypesSpec.hs
+++ test/TypesSpec.hs
@@ -7,17 +7,15 @@ import Test.QuickCheck
 
 spec :: Spec
 spec = describe "types" $ do
-  describe "epsilon" $
-    describe "epsilon" $ do
-      it "returns an Epsilon when the input is positive" $ property $
+  describe "epsilon" $ do
+      prop "returns an Epsilon when the input is positive" $
         \(Positive e) -> isRight (epsilon e)
 
       it "returns an error when the input is not positive" $ property $
         \(NonNegative e) -> (epsilon (-1 * e)) `shouldBe` Left (NonPositiveEpsilon (-1 * e))
 
-  describe "delta" $
-    describe "delta" $ do
-      it "returns a Delta when the input is not negative" $ property $
+  describe "delta" $ do
+      prop "returns a Delta when the input is not negative" $
         \(NonNegative d) -> isRight (delta d)
 
       it "returns an error when the input is negative" $ property $
