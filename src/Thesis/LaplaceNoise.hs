0d7d7c9a8a762db27462605fe9219965278ab747
Leendert van Doorn <xbt504@alumni.ku.dk>
2019-04-18 10:30:05 +0200

    Add haddock documentation

--- src/Thesis/LaplaceNoise.hs
+++ src/Thesis/LaplaceNoise.hs
@@ -1,7 +1,10 @@
+-- |Generates Laplace noise
 module Thesis.LaplaceNoise where
 
 import System.Random (StdGen, random)
 
+-- |Generates a random variable drawn from a Laplace distribution with the given scale using the
+-- given random number generator.
 generate :: StdGen -> Double -> (Double, StdGen)
 generate gen scale =
   let (rand, newGen) = random gen     -- value in [0, 1)
@@ -10,6 +13,7 @@ generate gen scale =
       noise = uniformToLaplace scale 0 uniform
   in (noise, newGen)
 
--- uniform is assumed to be a random variable drawn from a uniform distribution in (-0.5, 0.5]
+-- |Transforms a random variable drawn from a uniform distribution in (-0.5, 0.5] into a variable
+-- with distribution Laplace(mean, scale). This function contains no checks on the input variable.
 uniformToLaplace :: Double -> Double -> Double -> Double
 uniformToLaplace scale mean uniform = mean - scale * signum uniform * log(1 - 2 * abs uniform)
