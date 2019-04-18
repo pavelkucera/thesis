1c8f90a42d6faf2bb0e49a7fb72f6a36a4f96c7f
Pavel Kučera <pave.kucera@gmail.com>
2019-04-18 09:56:07 +0200

    Initialize query AST

---
+++ src/Thesis/Ast.hs
@@ -0,0 +1,35 @@
+{-# LANGUAGE GADTs #-}
+
+module Thesis.Ast where
+
+import Data.Text (Text)
+import Data.Typeable (Typeable)
+import Database.PostgreSQL.Simple.ToField (ToField)
+
+type Identifier = Text
+
+data Value where
+  Value :: (Eq a, ToField a, Show a, Typeable a) => a -> Value
+
+data Expr =
+    Literal Value
+  | Column Identifier
+  | PrefixOp Identifier Expr
+  | PostfixOp Identifier Expr
+  | BinaryOp Expr Identifier Expr
+  | FunctionCall Identifier [Expr]
+
+data CountExpr =
+    Star
+  | CountExpr Expr
+
+data Aggregation where
+  Average :: Expr -> Aggregation
+  Sum :: Expr -> Aggregation
+  Count :: CountExpr -> Aggregation
+
+data Select = Select {
+  selectAggregation :: Aggregation,
+  selectFrom :: Identifier,
+  selectWhere :: Maybe Expr
+}
