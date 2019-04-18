4b6540558ff040d4c9587d1d00fcdc141891ec44
Leendert van Doorn <xbt504@alumni.ku.dk>
2019-04-18 10:04:05 +0200

    Split Laplace noise generator into two functions

--- src/Thesis/LaplaceNoise.hs
+++ src/Thesis/LaplaceNoise.hs
@@ -7,5 +7,9 @@ generate gen scale =
   let (rand, newGen) = random gen     -- value in [0, 1)
       flippedUniform = rand - 0.5     -- value in [-0.5, 0.5)
       uniform = negate flippedUniform -- value in (-0.5, 0.5]
-      noise = scale * signum uniform * log (1 - 2 * abs uniform)
+      noise = uniformToLaplace scale 0 uniform
   in (noise, newGen)
+
+-- uniform is assumed to be a random variable drawn from a uniform distribution in (-0.5, 0.5]
+uniformToLaplace :: Double -> Double -> Double -> Double
+uniformToLaplace scale mean uniform = mean - scale * signum uniform * log(1 - 2 * abs uniform)
--- test/Thesis/LaplaceNoiseSpec.hs
+++ test/Thesis/LaplaceNoiseSpec.hs
@@ -9,8 +9,11 @@ import Thesis.LaplaceNoise
 
 spec :: Spec
 spec =
-  describe "Thesis.LaplaceNoise" $ do
-    prop "generates noise" $
-      \seed scale ->
-        let (noise, _) = generate (mkStdGen seed) scale
-        in abs noise `shouldSatisfy` (>= 0)
+  describe "Thesis.LaplaceNoise" $
+    describe "uniformToLaplace" $ do
+      prop "always transforms 0 into 0" $
+        \(scale, mean) -> uniformToLaplace scale mean 0 `shouldBe` mean
+      prop "generates noise" $
+        \seed scale ->
+          let (noise, _) = generate (mkStdGen seed) scale
+          in abs noise `shouldSatisfy` (>= 0)
