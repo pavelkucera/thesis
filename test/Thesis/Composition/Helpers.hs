d4085fd2573167b86ce898ca0f850ac94aca1ded
Pavel Kučera <pave.kucera@gmail.com>
2019-05-01 14:55:03 +0200

    Replace composition functions by privacy filters

---
+++ test/Thesis/Composition/Helpers.hs
@@ -0,0 +1,21 @@
+module Thesis.Composition.Helpers where
+
+import Thesis.Types
+import Thesis.Composition.PrivacyFilter
+import Thesis.ValueGuard
+
+mkBudget :: Epsilon -> Delta -> Budget
+mkBudget epsilon delta =
+  case (nonNegative epsilon, nonNegative delta) of
+    (Right epsilon, Right delta) -> (epsilon, delta)
+
+mkPrice :: Epsilon -> Delta -> QueryPrice
+mkPrice epsilon delta =
+  case (positive epsilon, nonNegative delta) of
+    (Right epsilon, Right delta) -> (epsilon, delta)
+
+countQueries :: PrivacyFilter a => a -> QueryPrice -> Int
+countQueries initialState price = loop 0 (Right initialState)
+ where
+  loop i (Left _) = i - 1
+  loop i (Right state) = loop (i + 1) $ subtractBudget state price
---
+++ test/Thesis/Composition/SimpleSpec.hs
@@ -0,0 +1,57 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+
+module Thesis.Composition.SimpleSpec (spec) where
+
+import Test.Hspec
+import Test.Hspec.QuickCheck
+import qualified Test.QuickCheck as QC
+import Thesis.Composition.PrivacyFilter
+import Thesis.Composition.Simple
+import Thesis.Types
+import Thesis.ValueGuard
+import Thesis.Composition.Helpers
+
+spec :: Spec
+spec = do
+  it "returns an error when epsilon exceeds the budget" $
+    let state = emptyState (mkBudget 1 2) :: SimpleCompositionState
+        price = mkPrice 2 1
+    in subtractBudget state price `shouldBe` Left BudgetDepleted
+
+  it "returns an error when delta exceeds the budget" $
+    let state = emptyState (mkBudget 2 1) :: SimpleCompositionState
+        price = mkPrice 1 2
+    in subtractBudget state price `shouldBe` Left BudgetDepleted
+
+  it "returns updated budget" $
+    let state = emptyState (mkBudget 2 2)
+        price = mkPrice 1 1
+        (epsilon, delta) = mkBudget 1 1
+    in subtractBudget state price `shouldBe` Right (SimpleCompositionState epsilon delta)
+
+  it "allows for a series of queries" $
+    let state = emptyState (mkBudget 0.5 (2**(-30))) :: SimpleCompositionState
+        price = mkPrice (2**(-11)) 0
+    in countQueries state price `shouldBe` 1024
+
+  prop "returns a smaller budget for a random epsilon" $
+    \(QC.Positive (epsilon1 :: Epsilon)) (QC.Positive (epsilon2 :: Epsilon)) ->
+      let initialEpsilon = max epsilon1 epsilon2
+          queryEpsilon = min epsilon1 epsilon2
+          budget = mkBudget initialEpsilon 1
+          price = mkPrice queryEpsilon 1
+          state = emptyState budget
+          newBudget = subtractBudget state price
+          Right (SimpleCompositionState newEpsilon _) = newBudget
+      in value newEpsilon < initialEpsilon
+
+  prop "returns a smaller budget for a random epsilon" $
+    \(QC.Positive (delta1 :: Delta)) (QC.Positive (delta2 :: Delta)) ->
+      let initialDelta = max delta1 delta2
+          queryDelta = min delta1 delta2
+          budget = mkBudget 1 initialDelta
+          price = mkPrice 1 queryDelta
+          state = emptyState budget
+          newState = subtractBudget state price
+          Right (SimpleCompositionState _ newDelta) = newState
+      in value newDelta < initialDelta
