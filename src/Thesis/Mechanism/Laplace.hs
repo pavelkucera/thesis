84dcc31d988785c5c4b326e08732684a45238190
Pavel Kučera <pave.kucera@gmail.com>
2019-05-06 10:54:04 +0200

    Rewrite Laplace mechanism to run negated queries

--- src/Thesis/Mechanism/Laplace.hs
+++ src/Thesis/Mechanism/Laplace.hs
@@ -1,30 +1,33 @@
 module Thesis.Mechanism.Laplace where
 
 import Control.Monad.IO.Class (MonadIO)
-import Control.Monad.Catch (MonadMask)
-import Data.Time.Units (TimeUnit)
 import Database.PostgreSQL.Simple (Connection)
 import System.Random (StdGen)
 
 import Thesis.Ast
-import Thesis.Composition.PrivacyFilter
 import Thesis.LaplaceNoise (generate)
-import Thesis.Microtransaction (runMicrotransaction)
 import Thesis.Query
 import Thesis.SqlGenerator (generateSql)
 import Thesis.SqlRunner (executeSql)
+import Thesis.Transformer
 import Thesis.ValueGuard
 
-laplace :: (TimeUnit t, MonadIO m, MonadMask m) => StdGen -> Connection -> Query -> t -> m (StdGen, Either BudgetDepleted Double)
-laplace gen conn query timeout =
-  let sqlQuery = generateSql (queryAst query)
-      (defaultAns, tempGen) = defaultAnswer gen query
-      scale = value (queryEpsilon query) / getSensitivity (fst $ selectAggregation (queryAst query))
-      (noise, newGen) = generate tempGen scale
-      computation = executeSql conn sqlQuery
+laplace :: (MonadIO m)
+        => StdGen
+        -> Connection
+        -> Query
+        -> m (StdGen, Double)
+laplace gen connection query =
+  let negatedQuery = negateQuery query
+      sensitivity = getSensitivity (fst $ selectAggregation (queryAst query))
+      noiseScale = value (queryEpsilon query) / sensitivity
+      (noise, newGen) = generate gen noiseScale
   in do
-    transactionResult <- runMicrotransaction timeout defaultAns computation
-    return (newGen, Right $ transactionResult + noise)
+    result <- execute connection query
+    _ <- execute connection negatedQuery
+    return (newGen, result + noise)
 
-defaultAnswer :: StdGen -> Query -> (Double, StdGen)
-defaultAnswer gen _ = (1337, gen)
+execute :: MonadIO m => Connection -> Query -> m Double
+execute connection query =
+  let sql = generateSql $ queryAst query
+  in executeSql connection sql
