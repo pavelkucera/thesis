40314d9847ad6c9be02cff971651e69a97429aed
Leendert van Doorn <xbt504@alumni.ku.dk>
2019-05-17 13:56:32 +0200

    Move generation of noise to Mechanism.Laplace

--- src/Thesis/Mechanism/Laplace.hs
+++ src/Thesis/Mechanism/Laplace.hs
@@ -2,10 +2,9 @@ module Thesis.Mechanism.Laplace where
 
 import Control.Monad.IO.Class (MonadIO)
 import Database.PostgreSQL.Simple (Connection)
-import System.Random (StdGen)
+import System.Random (StdGen, random)
 
 import Thesis.Ast
-import Thesis.LaplaceNoise
 import Thesis.Query
 import Thesis.SqlGenerator (emitLaplace)
 import Thesis.SqlRunner (executeSql)
@@ -25,3 +24,21 @@ laplace gen connection (DatabaseQuery e ast@(DatabaseSelect (agg, _) _ _)) =
   in do
     trueAnswer <- executeSql connection sql
     return (newGen, trueAnswer + noise)
+
+-- | Generates a random variable drawn from a Laplace distribution with the given scale using the
+-- given random number generator.
+generate :: StdGen -> Double -> (Double, StdGen)
+generate gen scale =
+  let (rand, newGen) = random gen     -- value in [0, 1)
+      flippedUniform = rand - 0.5     -- value in [-0.5, 0.5)
+      uniform = negate flippedUniform -- value in (-0.5, 0.5]
+      noise = uniformToLaplace scale 0 uniform
+  in (noise, newGen)
+
+-- | Transforms a random variable drawn from a uniform distribution in (-0.5, 0.5] into a variable
+-- with distribution Laplace(mean, scale). This function contains no checks on the input variable.
+uniformToLaplace :: Double -- ^ The scale of the Laplace distribution
+                 -> Double -- ^ The mean of the Laplace distribution
+                 -> Double -- ^ A random variable drawn uniformly from (-0.5, 0.5]
+                 -> Double -- ^ A random value drawn from Laplace(mean, scale)
+uniformToLaplace scale mean uniform = mean - scale * signum uniform * log(1 - 2 * abs uniform)
