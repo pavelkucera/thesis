5efbfe02f5197667af13a2db475beb50e4f963a3
Pavel Kučera <pave.kucera@gmail.com>
2019-05-17 20:23:23 +0200

    Remove unused function

--- src/Thesis/SqlRunner.hs
+++ src/Thesis/SqlRunner.hs
@@ -1,6 +1,5 @@
 module Thesis.SqlRunner (
   executeSql,
-  executeSqlList,
   extractValue
 ) where
 
@@ -17,12 +16,6 @@ executeSql conn sqlPart = do
   return $ case queryResult of
     [Only (Just v)] -> toRealFloat v
     _ -> 0
-
-executeSqlList :: (MonadIO m) => Connection -> SqlPart -> m [Double]
-executeSqlList conn sqlPart = do
-  let (sqlQuery, sqlParameters) = toQuery sqlPart
-  queryResult <- liftIO $ query conn sqlQuery sqlParameters
-  return $ extractValue `map` queryResult
 
 extractValue :: Only (Maybe Scientific) -> Double
 extractValue (Only (Just v)) = toRealFloat v
