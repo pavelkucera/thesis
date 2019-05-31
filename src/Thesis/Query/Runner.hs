c63cdfb68f5dc0671df9c6cf72ba78f7ad54bd14
Leendert van Doorn <xbt504@alumni.ku.dk>
2019-05-31 09:53:57 +0200

    Add the function runString

--- src/Thesis/Query/Runner.hs
+++ src/Thesis/Query/Runner.hs
@@ -3,12 +3,15 @@
 module Thesis.Query.Runner where
 
 import Control.Monad.IO.Class (MonadIO)
+import Data.Void (Void)
 import Database.PostgreSQL.Simple (Connection)
 import System.Random (StdGen)
+import Text.Megaparsec.Error (ParseErrorBundle)
 
 import Thesis.Composition.PrivacyFilter
 import Thesis.Mechanism.Exponential
 import Thesis.Mechanism.Laplace
+import Thesis.Parser (parseQuery)
 import Thesis.Query.Ast
 import Thesis.Query.Query
 import Thesis.Types
@@ -32,3 +35,19 @@ run gen conn privacyFilter (Query e aggregation) =
     Right newState -> do
       (newGen, res) <- mechanism
       return (newState, newGen, Right res)
+
+runString :: (MonadIO m, PrivacyFilter p)
+          => StdGen
+          -> Connection
+          -> p
+          -> Positive Epsilon
+          -> String
+          -> m (p, StdGen, Either (Either BudgetDepleted (ParseErrorBundle String Void)) Double)
+runString gen conn privacyFilter e queryString =
+  case (parseQuery queryString) of
+    Right aggregation -> do
+      (newState, newGen, queryResult) <- run gen conn privacyFilter (Query e aggregation)
+      case queryResult of
+        Right res -> return (newState, newGen, Right res)
+        Left err -> return (newState, newGen, Left (Left err))
+    Left err -> return (privacyFilter, gen, Left (Right err))
