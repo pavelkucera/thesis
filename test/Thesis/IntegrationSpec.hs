1182a4510f73860e04b0490b5c26cca6d3f93377
Pavel Kučera <pave.kucera@gmail.com>
2019-06-05 10:17:12 +0200

    Use high-level interface in integration tests

--- test/Thesis/IntegrationSpec.hs
+++ test/Thesis/IntegrationSpec.hs
@@ -10,11 +10,7 @@ import System.Environment (lookupEnv)
 import System.Random
 import Test.Hspec
 
-import Thesis.Composition.Simple
-import Thesis.Query.Ast
-import Thesis.Query.Query
-import Thesis.Query.Runner (run)
-import Thesis.ValueGuard (positive, nonNegative, zero)
+import Thesis
 
 seed :: Int
 seed = 1337
@@ -42,37 +38,28 @@ spec = do
         privacyFilter = SimpleCompositionState budgetEpsilon zero
 
     it "counts using the Laplace mechanism" $
-      \(connection, gen) ->
-        let countAst = DatabaseAggregation Count $ AggregationAst (Column "income") "households" Nothing
-            countQuery = Query qEpsilon countAst
-        in do
-          (_, _, output) <- run gen connection privacyFilter countQuery
-          -- Real count is 9,
-          -- With this seed the first generated number is 0.23132540861101902
-          -- Transformed to laplace noise as 0.7707826838949735
-          -- Final answer must be 9.7707826838949735
-          output `shouldBe` Right 9.7707826838949735
+      \(connection, gen) -> do
+        (_, _, output) <- runString gen connection privacyFilter qEpsilon "SELECT COUNT(income) FROM households"
+        -- Real count is 9,
+        -- With this seed the first generated number is 0.23132540861101902
+        -- Transformed to laplace noise as 0.7707826838949735
+        -- Final answer must be 9.7707826838949735
+        output `shouldBe` Right 9.7707826838949735
 
     it "sums using the Laplace mechanism" $
-      \(connection, gen) ->
-        let sumAst = DatabaseAggregation Sum $ AggregationAst (Column "income") "households" Nothing
-            sumQuery = Query qEpsilon sumAst
-        in do
-          (_, _, output) <- run gen connection privacyFilter sumQuery
-          -- Real sum is 5.4
-          -- With this seed the first generated number is 0.23132540861101902
-          -- Transformed to laplace noise as 0.7707826838949735
-          -- Final answer must be 6.1707826838949735
-          output `shouldBe` Right 6.1707826838949735
+      \(connection, gen) -> do
+        (_, _, output) <- runString gen connection privacyFilter qEpsilon "SELECT SUM(income) FROM households"
+        -- Real sum is 5.4
+        -- With this seed the first generated number is 0.23132540861101902
+        -- Transformed to laplace noise as 0.7707826838949735
+        -- Final answer must be 6.1707826838949735
+        output `shouldBe` Right 6.1707826838949735
 
     it "calculates averages using the Laplace mechanism" $
-      \(connection, gen) ->
-        let avgAst = DatabaseAggregation Average $ AggregationAst (Column "income") "households" Nothing
-            avgQuery = Query qEpsilon avgAst
-        in do
-          (_, _, output) <- run gen connection privacyFilter avgQuery
-          -- Real average is 0.6
-          -- With this seed the first generated number is 0.23132540861101902
-          -- Transformed to laplace noise as 0.7707826838949735
-          -- Final answer must be 1.3707826838949735
-          output `shouldBe` Right 1.3707826838949735
+      \(connection, gen) -> do
+        (_, _, output) <- runString gen connection privacyFilter qEpsilon "SELECT AVG(income) FROM households"
+        -- Real average is 0.6
+        -- With this seed the first generated number is 0.23132540861101902
+        -- Transformed to laplace noise as 0.7707826838949735
+        -- Final answer must be 1.3707826838949735
+        output `shouldBe` Right 1.3707826838949735
