433c0fd3fa4c7b1878f97d5100cf27f541b4c214
Pavel Kučera <pave.kucera@gmail.com>
2019-06-05 09:21:11 +0200

    Run CI tests against a docker database

--- test/Thesis/IntegrationSpec.hs
+++ test/Thesis/IntegrationSpec.hs
@@ -2,8 +2,11 @@
 
 module Thesis.IntegrationSpec (spec) where
 
-import Data.ByteString
+import Control.Exception (bracket)
+import Data.ByteString.Char8 (pack)
+import Data.Maybe (fromMaybe)
 import Database.PostgreSQL.Simple
+import System.Environment (lookupEnv)
 import System.Random
 import Test.Hspec
 
@@ -16,50 +19,62 @@ import Thesis.ValueGuard (positive, nonNegative, zero)
 seed :: Int
 seed = 1337
 
-connStr :: ByteString
-connStr = "host=localhost dbname=postgres user=postgres password=password"
+openConnection :: IO Connection
+openConnection = do
+  envHost <- lookupEnv "POSTGRES_HOST"
+  let host = fromMaybe "localhost" envHost
+
+  connectPostgreSQL . pack $ "host=" ++ host ++ " dbname=postgres user=postgres password=password"
+
+closeConnection :: Connection -> IO ()
+closeConnection = close
+
+withConnection :: (Connection -> IO ()) -> IO ()
+withConnection = bracket openConnection closeConnection
 
 spec :: Spec
 spec = do
-  describe "run" $ do
-    let (qEpsilon, budgetEpsilon) = case (positive 1, nonNegative 1) of
-                                      (Right e1, Right e2) -> (e1, e2)
+  around withConnection $ describe "run" $ do
+    let (qEpsilon, budgetEpsilon) =
+          case (positive 1, nonNegative 1) of
+            (Right e1, Right e2) -> (e1, e2)
         privacyFilter = SimpleCompositionState budgetEpsilon zero
+
     it "counts using the Laplace mechanism" $
-      let countAst = DatabaseAggregation Count $ AggregationAst (Column "total_household_income") "households" Nothing
-          countQuery = Query qEpsilon countAst
-          gen = mkStdGen seed
-      in do
-        conn <- connectPostgreSQL connStr
-        (_, _, output) <- run gen conn privacyFilter countQuery
-        -- Real count is 41544,
-        -- With this seed the first generated number is 0.23132540861101902
-        -- Transformed to laplace noise as 0.7707826838949735
-        -- Final answer must be 41544.7707826838949735
-        output `shouldBe` Right 41544.7707826838949735
+      \connection ->
+        let countAst = DatabaseAggregation Count $ AggregationAst (Column "income") "households" Nothing
+            countQuery = Query qEpsilon countAst
+            gen = mkStdGen seed
+        in do
+          (_, _, output) <- run gen connection privacyFilter countQuery
+          -- Real count is 9,
+          -- With this seed the first generated number is 0.23132540861101902
+          -- Transformed to laplace noise as 0.7707826838949735
+          -- Final answer must be 9.7707826838949735
+          output `shouldBe` Right 9.7707826838949735
 
     it "sums using the Laplace mechanism" $
-      let sumAst = DatabaseAggregation Sum $ AggregationAst (Column "total_household_income") "households" Nothing
-          sumQuery = Query qEpsilon sumAst
-          gen = mkStdGen seed
-      in do
-        conn <- connectPostgreSQL connStr
-        (_, _, output) <- run gen conn privacyFilter sumQuery
-        -- Real sum is -39803.23160805511989369339
-        -- With this seed the first generated number is 0.23132540861101902
-        -- Transformed to laplace noise as 0.7707826838949735
-        -- Final answer must be -39802.46082537122
-        output `shouldBe` Right (-39802.46082537122)
+      \connection ->
+        let sumAst = DatabaseAggregation Sum $ AggregationAst (Column "income") "households" Nothing
+            sumQuery = Query qEpsilon sumAst
+            gen = mkStdGen seed
+        in do
+          (_, _, output) <- run gen connection privacyFilter sumQuery
+          -- Real sum is 5.4
+          -- With this seed the first generated number is 0.23132540861101902
+          -- Transformed to laplace noise as 0.7707826838949735
+          -- Final answer must be 6.1707826838949735
+          output `shouldBe` Right 6.1707826838949735
 
     it "calculates averages using the Laplace mechanism" $
-      let avgAst = DatabaseAggregation Average $ AggregationAst (Column "total_household_income") "households" Nothing
-          avgQuery = Query qEpsilon avgAst
-          gen = mkStdGen seed
-      in do
-        conn <- connectPostgreSQL connStr
-        (_, _, output) <- run gen conn privacyFilter avgQuery
-        -- Real average is -0.95809819969321971629
-        -- With this seed the first generated number is 0.23132540861101902
-        -- Transformed to laplace noise as 0.7707826838949735
-        -- Final answer must be -0.18731551579824623
-        output `shouldBe` Right (-0.18731551579824623)
+      \connection ->
+        let avgAst = DatabaseAggregation Average $ AggregationAst (Column "income") "households" Nothing
+            avgQuery = Query qEpsilon avgAst
+            gen = mkStdGen seed
+        in do
+          (_, _, output) <- run gen connection privacyFilter avgQuery
+          -- Real average is 0.6
+          -- With this seed the first generated number is 0.23132540861101902
+          -- Transformed to laplace noise as 0.7707826838949735
+          -- Final answer must be 6.7707826838949735
+          output `shouldBe` Right 1.3707826838949735
