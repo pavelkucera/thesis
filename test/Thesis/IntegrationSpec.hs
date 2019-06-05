c811a2b33d7817ec5cb5b3541911f50a7d131f3c
Pavel Kučera <pave.kucera@gmail.com>
2019-06-05 10:26:58 +0200

    Use hooks to setup test budget

--- test/Thesis/IntegrationSpec.hs
+++ test/Thesis/IntegrationSpec.hs
@@ -4,6 +4,7 @@ module Thesis.IntegrationSpec (spec) where
 
 import Control.Exception (bracket)
 import Data.ByteString.Char8 (pack)
+import Data.Either (fromRight)
 import Data.Maybe (fromMaybe)
 import Database.PostgreSQL.Simple
 import System.Environment (lookupEnv)
@@ -15,30 +16,27 @@ import Thesis
 seed :: Int
 seed = 1337
 
-initState :: IO (Connection, StdGen)
+initState :: IO (Connection, StdGen, Positive Epsilon, SimpleCompositionState)
 initState = do
   envHost <- lookupEnv "POSTGRES_HOST"
   let host = fromMaybe "localhost" envHost
       gen = mkStdGen seed
+      budgetEpsilon = fromRight (error "negative epsilon") $ nonNegative 1
+      queryEpsilon = fromRight (error "non-positive epsilon") $ positive 1
   connection <- connectPostgreSQL . pack $ "host=" ++ host ++ " dbname=postgres user=postgres password=password"
-  return (connection, gen)
+  return (connection, gen, queryEpsilon, SimpleCompositionState budgetEpsilon zero)
 
-destroyState :: (Connection, StdGen) -> IO ()
-destroyState = close . fst
+destroyState :: (Connection, StdGen, Positive Epsilon, SimpleCompositionState) -> IO ()
+destroyState (connection, _, _, _) = close connection
 
-withConnection :: ((Connection, StdGen) -> IO ()) -> IO ()
+withConnection :: ((Connection, StdGen, Positive Epsilon, SimpleCompositionState) -> IO ()) -> IO ()
 withConnection = bracket initState destroyState
 
 spec :: Spec
 spec = do
   around withConnection $ describe "run" $ do
-    let (qEpsilon, budgetEpsilon) =
-          case (positive 1, nonNegative 1) of
-            (Right e1, Right e2) -> (e1, e2)
-        privacyFilter = SimpleCompositionState budgetEpsilon zero
-
     it "counts using the Laplace mechanism" $
-      \(connection, gen) -> do
+      \(connection, gen, qEpsilon, privacyFilter) -> do
         (_, _, output) <- runString gen connection privacyFilter qEpsilon "SELECT COUNT(income) FROM households"
         -- Real count is 9,
         -- With this seed the first generated number is 0.23132540861101902
@@ -47,7 +45,7 @@ spec = do
         output `shouldBe` Right 9.7707826838949735
 
     it "sums using the Laplace mechanism" $
-      \(connection, gen) -> do
+      \(connection, gen, qEpsilon, privacyFilter) -> do
         (_, _, output) <- runString gen connection privacyFilter qEpsilon "SELECT SUM(income) FROM households"
         -- Real sum is 5.4
         -- With this seed the first generated number is 0.23132540861101902
@@ -56,7 +54,7 @@ spec = do
         output `shouldBe` Right 6.1707826838949735
 
     it "calculates averages using the Laplace mechanism" $
-      \(connection, gen) -> do
+      \(connection, gen, qEpsilon, privacyFilter) -> do
         (_, _, output) <- runString gen connection privacyFilter qEpsilon "SELECT AVG(income) FROM households"
         -- Real average is 0.6
         -- With this seed the first generated number is 0.23132540861101902
